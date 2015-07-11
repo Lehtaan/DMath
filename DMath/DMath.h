99c89357daac403f9748de33f1b70f07e5487901
Janis Böhm <janis@janiss-mbp.speedport_w_724v_typ_a_05011603_00_003>
2015-07-11 15:54:15 -0700

    Initial Commit

---
+++ DMath/DMath.h
@@ -0,0 +1,13 @@
+//
+//  DMath.h
+//  DMath
+//
+//  Created by Janis Böhm on 11/07/15.
+//  Copyright (c) 2015 Janis Böhm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DMath : NSObject
+
+@end
---
+++ DMath/DMath.m
@@ -0,0 +1,13 @@
+//
+//  DMath.m
+//  DMath
+//
+//  Created by Janis Böhm on 11/07/15.
+//  Copyright (c) 2015 Janis Böhm. All rights reserved.
+//
+
+#import "DMath.h"
+
+@implementation DMath
+
+@end
